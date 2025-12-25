4ef163490556d3c8214f9842e875609ef5f05e64
loyalty1234 <loyaltyetejerhe@gmail.com>
2025-12-26 00:08:18 +0100

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <Link className='logo' href={'/'}>
+        <Image src={'/icons/logo.png'} alt='logo' width={24} height={24}/>
+
+        <p>DevEvent</p>
+        </Link>
+
+        <ul>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/events'}>Events</Link>
+          <Link href={'/'}>Create Event</Link>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { title } from "process"
+import ExploreBtn from "./components/ExploreBtn"
+import EventCard from "./components/EventCard"
+import { events } from "@/lib/constants"
+
+const Home = () => {
+  return (
+    <section>
+      <h1 className="text-center">The Hub for Every Dev. <br /> Events you can't miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and confrences, All in one place.</p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li className="list-none" key={event.title}>
+              <EventCard {...event}
+              />
+            </li>
+          )
+          )}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Home 
